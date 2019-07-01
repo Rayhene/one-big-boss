3f1e85a8eccf4f25927584a9fd3174cd66891d04
Rayhene <42323511+Rayhene@users.noreply.github.com>
2019-07-01 17:47:53 -0300

    second commit

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -10,9 +10,16 @@ public class Boss : MonoBehaviour
 
     private Transform player;
 
+    public GameObject projectile;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
     }
 
     
@@ -29,6 +36,15 @@ public class Boss : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
 
+        }
+
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }else
+        {
+            timeBtwShots -= Time.deltaTime;
         }
     }
 }
