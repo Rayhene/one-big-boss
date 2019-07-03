13f76c36ee2ce614b259b69ce6b211754ee5b0cb
Quote023 <alexandresouzape@gmail.com>
2019-07-02 21:16:52 -0300

    Movimentação em relação ao mouse
    Player.CS
    InputManager

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public float speed;
     private Vector2 moveVelocity;
     private Rigidbody2D rb;
+    public GameObject Arma;
 
  
     void Start()
@@ -18,11 +19,31 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveVelocity = moveInput.normalized * speed; 
+        moveVelocity = moveInput.normalized * speed;
+        RotateChar();
+
+
     }
 
      void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
     }
-}
+
+    void RotateChar() //Rotacionar a Arma
+    {
+
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        Vector3 VecDir = transform.position - mouseWorld;
+        VecDir = VecDir.normalized;
+        float dir = Vector2.SignedAngle(new Vector2(-1,0), VecDir);
+        Debug.Log("dir " + dir);
+        Debug.Log("dir " + VecDir);
+
+        //  float dir = Mathf.Atan2(mouseWorld.y, mouseWorld.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, dir));
+
+    }
+
+    }
+
